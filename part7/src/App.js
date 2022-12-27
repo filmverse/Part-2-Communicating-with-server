d683d8730256d192f65feb2b54ce7e3af57c3385
vikas <vkassharma19492@gmail.com>
2022-12-27 13:52:43 +0530

    part7 has been updated

--- part7/src/App.js
+++ part7/src/App.js
@@ -1,17 +1,5 @@
 
-import { useState, useEffect } from "react";
-import axios from 'axios';
-
 const App = () => {
-
-  const hook = () => {
-    axios.get('https://restcountries.com/v3.1/all').then(response => {
-      console.log(response.data)
-    })
-  }
-
-  useEffect(hook, [])
-
   return (
     <div>
       <h1>Hello World</h1>
