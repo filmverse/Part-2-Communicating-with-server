93e88259b09f6abdb38a65427a35ce288dd3d2a6
vikas <vkassharma19492@gmail.com>
2022-12-29 02:38:26 +0530

    part7 has been updated

--- part7/src/App.js
+++ part7/src/App.js
@@ -4,14 +4,18 @@ import axios from "axios";
 
 const App = () => {
 
-  const [countries, setCountries] = useState([])
+  const [ countries, setCountries ] = useState([])
+  const [ query, setQuery ] = useState('')
 
   const dev = () => {
     axios.get('https://restcountries.com/v3.1/all').then(response => {
       console.log(response.data)
       setCountries(response.data.map(cname => ({
         name: cname.name.common,
-        capital: cname.capital
+        capital: cname.capital,
+        area: cname.area,
+        language: cname.languages,
+        flag: cname.flag,
       })))
     })
   }
@@ -20,9 +24,14 @@ const App = () => {
 
   console.log(countries)
 
+  const handleChange = (event) => {
+    setQuery(event.target.value)
+  }
+
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1>find countries <input value={query} onChange={handleChange} /></h1>
+      {countries.map((country) => <h1 key={country.name}>name:{country.name}<br />capital:{country.capital}<br />flag:{country.flag}</h1>)}
     </div>
   )
 }
