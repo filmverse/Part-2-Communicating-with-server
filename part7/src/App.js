b53496ef56f06287837e2d7f0c70b5033c889be2
vikas <vkassharma19492@gmail.com>
2022-12-29 02:11:32 +0530

    part7 has been updated

--- part7/src/App.js
+++ part7/src/App.js
@@ -1,8 +1,28 @@
 
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 const App = () => {
+
+  const [countries, setCountries] = useState([])
+
+  const dev = () => {
+    axios.get('https://restcountries.com/v3.1/all').then(response => {
+      console.log(response.data)
+      setCountries(response.data.map(cname => ({
+        name: cname.name.common,
+        capital: cname.capital
+      })))
+    })
+  }
+
+  useEffect(dev, [])
+
+  console.log(countries)
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello World!</h1>
     </div>
   )
 }
