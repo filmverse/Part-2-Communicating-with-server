58e6ba5d0678613388224a1befbb634aaecbc0b4
vikas <vkassharma19492@gmail.com>
2023-01-02 02:14:14 +0530

    part7 has been updated

--- part7/src/components/CountryDetails.js
+++ part7/src/components/CountryDetails.js
@@ -1,6 +1,23 @@
 
+import { useState } from "react";
 import axios from "axios";
+import kelvinToCelsius from "../utils/kelvinToCelsius";
+
 const CountryDetails = ({ country }) => {
+
+    const [ temperature, setTemperature ] = useState(0)
+    const [ wind, setWind ] = useState(0)
+    const [ icon, setIcon ] = useState('')
+
+    axios.get(`
+        https://api.openweathermap.org/data/2.5/weather?q=${country.capital[0]}&appid=${process.env.REACT_APP_API_KEY}
+    `).then(response => {
+        console.log(response.data)
+        setTemperature(Math.round(kelvinToCelsius(response.data.main.temp) * 10) / 10)
+        setWind(response.data.wind.speed)
+        setIcon(response.data.weather[0].icon)
+    })
+
     return (
         <div>
             <h1>{country.name}</h1>
@@ -15,6 +32,10 @@ const CountryDetails = ({ country }) => {
             <div>
                 <img src={country.flags.png} alt={`${country.name} flag`} />
             </div>
+            <h2>Weather in {country.capital[0]}</h2>
+            <div>temperature {temperature} Celsius</div>
+            <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="" />
+            <div>wind {wind} m/s</div>
         </div>
     )
 }
