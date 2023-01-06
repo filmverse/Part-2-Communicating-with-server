b250dcaf74752e1fe07b05940e0f830ded881332
vikas <vkassharma19492@gmail.com>
2023-01-07 00:45:23 +0530

    part 5 has been updated

--- part5/src/App.js
+++ part5/src/App.js
@@ -26,12 +26,17 @@ const App = () => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
-      date: new Date().toISOString(),
+      date: new Date(),
       important: Math.random() < 0.5,
-      id: notes.length + 1,
     }
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    axios.post('http://localhost:3001/notes', noteObject).then(
+      response => {
+        setNotes(notes.concat(response.data))
+        setNewNote('')
+      }
+    )
+    // setNotes(notes.concat(noteObject))
+    // setNewNote('')
   }
 
   const handleNoteChange = (event) => {
