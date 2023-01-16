d70a8191bfc35d6256b381d6c4eeed31a0a0fc5d
vikas <vkassharma19492@gmail.com>
2023-01-16 23:57:10 +0530

    parTS has been updated

--- part9/src/App.js
+++ part9/src/App.js
@@ -8,7 +8,7 @@ const App = () => {
     web3 = new Web3(window.ethereum || window.web3.currentProvider);
   } else {
     // Use a fallback provider (e.g. Infura)
-    web3 = new Web3(new Web3.providers.HttpProvider("https://goerli.infura.io/v3/6579bd6485d945559174aaa1ef4b4f71"));
+    web3 = new Web3(new Web3.providers.HttpProvider(//"WEB3 API KEY"//));
   }
 
   window.ethereum.enable().then(() => {
