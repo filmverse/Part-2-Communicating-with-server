d96af8c708efea89f586efab585e4d5aa1a4cf00
Vikas Sharma <110290268+filmverse@users.noreply.github.com>
2023-01-25 18:58:57 +0530

    Update Persons.js

--- part11/src/components/Persons.js
+++ part11/src/components/Persons.js
@@ -3,7 +3,7 @@ const Persons = ({ persons, query, removePerson }) => {
     return (
         <div>
             {persons
-                .filter(person => person.name.toLocaleLowerCase().includes(query))
+                .filter(person => person.name.toLowerCase().includes(query))
                 .map(person => <p key={person.name}>{person.name} {person.number} <button onClick={removePerson(person.id, person.name)}>delete</button></p>)}
         </div>
     )
